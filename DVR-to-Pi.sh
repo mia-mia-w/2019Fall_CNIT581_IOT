6e967122bb3b46841173e83ceef6288b0f7e2867
Lucas Hennessy <lhenness@purdue.edu>
2019-11-20 20:26:10 -0500

    Corrected variables; edited inotify to give the file which was created

--- DVR-to-Pi.sh
+++ DVR-to-Pi.sh
@@ -9,11 +9,11 @@ $EdgePassword = "<don't insert real password here ever on GitHub>"
 
 #----------------Main-----------------------------
 While ( $T = "true" ); Do
-	While (inotifywait -t 20 -r -e close_write $UniFiDir); Do # Checks recursively for 20 seconds if any file has been written and closed for any number of cameras
+	inotifywait -r -e close_write -t 20 $UniFiDir | While read $WrittenFile; Do # Checks recursively for 20 seconds if any file has been written and closed for any number of cameras
 	  $Date = date +%m-%d-%Y-%H-%M # Get date in a nice format
-	  Touch "~/$Date-motion-notification.txt" # Create notification message to alert the farmer rather than wait for the large video file to transfer
-	  Ftp -4 -i user $PiUser $PiPassword put "./$Date-motion-notification.txt" "$PiIP:/motion-notification" # FTP a notification of motion to the edge server to beginning notification of the farmer
-	  rm "./$Date-motion-notification.txt" # Clean up the local notification
-	  Ftp -4 -i user $PiUser $PiPassword put $file $PiIP:/motion # FTP the motion video file to the edge server
+	  Touch "~/$Date.txt" # Create notification message to alert the farmer rather than wait for the large video file to transfer
+	  Ftp -4 -i user $EdgeUser $EdgePassword put "./$Date.txt" "$EdgeServer:/motion-notification" # FTP a notification of motion to the edge server to beginning notification of the farmer
+	  rm "./$Date.txt" # Clean up the local notification
+	  Ftp -4 -i user $EdgeUser $EdgePassword put $WrittenFile $EdgeServer:/motion # FTP the motion video file to the edge server
 	Done
 Done
