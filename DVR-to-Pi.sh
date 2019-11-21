1ec0644396fbfc33197abf4508d6e7598a24b9fe
Lucas Hennessy <lhenness@purdue.edu>
2019-11-20 20:04:51 -0500

    Supports multiple cameras automagically

--- DVR-to-Pi.sh
+++ DVR-to-Pi.sh
@@ -1,44 +1,19 @@
 #!/bin/bash
 
-#----------------Variables------------------------
-$T = "false"
-  # always run script
-$FootageDir = "/etc/unifi"
-$CameraDir = "/var/lib/unifi-video/videos/dc200454-4f0a-301e-ac57-6bac105b9846"
-$ArchivedLiveDir = "/var/lib/unifi-video/videos/dc200454-4f0a-301e-ac57-6bac105b9846/Archived-Live"
-$ArchivedMotionDir = "/var/lib/unifi-video/videos/dc200454-4f0a-301e-ac57-6bac105b9846/Archived-Motion"
-$PiIP = "192.168.1.45"
-$PiUser = "pi"
-$PiPassword = "encrypted-piece-of-shit"
-
-#----------------Functions-------------------------
-CheckWhenDone () {
-  While ($ModifiedDate != Date -r "$file"); Do
-    $ModifiedDate = Date -r "$file"
-    Sleep 2
-  Done
-}
+#----------------Static-Variables------------------------
+$T = "false" # Meant to never end the script
+$UniFiDir = "/var/lib/unifi-video/videos"
+$EdgeServer = "192.168.1.45"
+$EdgeUser = "pi"
+$EdgePassword = "<don't insert real password here ever on GitHub>"
 
 #----------------Main-----------------------------
 While ( $T = "true" ); Do
-	$Year = date +%Y
-	$Month = date +%m
-	$Day = date +%d
-	$VideoDir = "$CameraDir/$Year/$Month/$Day"
-	While (inotifywait -t 20 -e create $VideoDir); Do
-	  # test forever for any newly created files in $FootageDir recursively and perform an action after
-	  $Date = date
-	  Touch "./$Date-motion-notification.txt"
-	    # Create notification message
-	  Ftp -4 -i user $PiUser $PiPassword put "./$Date-motion-notification.txt" "$PiIP:/motion-notification"
-	    # Send notification (aka the txt) to the pi to trigger an alert to be sent to the farmer
-	  rm "./$Date-motion-notification.txt"
-	    # Clean up local notification
-	  $ModifiedDate = Date -r "$file"
-	    # Get the last modified date of the newly created file
-	  Sleep 2
-	  CheckWhenDone()
-	  Ftp -4 -i user $PiUser $PiPassword put $file $PiIP:/motion/
-	  mv $File $OldMotionDir
+	While (inotifywait -t 20 -r -e close_write $UniFiDir); Do # Checks recursively for 20 seconds if any file has been written and closed for any number of cameras
+	  $Date = date +%m-%d-%Y-%H-%M # Get date in a nice format
+	  Touch "~/$Date-motion-notification.txt" # Create notification message to alert the farmer rather than wait for the large video file to transfer
+	  Ftp -4 -i user $PiUser $PiPassword put "./$Date-motion-notification.txt" "$PiIP:/motion-notification" # FTP a notification of motion to the edge server to beginning notification of the farmer
+	  rm "./$Date-motion-notification.txt" # Clean up the local notification
+	  Ftp -4 -i user $PiUser $PiPassword put $file $PiIP:/motion # FTP the motion video file to the edge server
 	Done
 Done
