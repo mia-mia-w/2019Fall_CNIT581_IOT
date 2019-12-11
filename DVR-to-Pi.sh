039d072c593ea15907f041395d8c997cc50b2e13
Lucas Hennessy <lhenness@purdue.edu>
2019-12-10 21:49:47 -0500

    Testing Wave1

--- DVR-to-Pi.sh
+++ DVR-to-Pi.sh
@@ -7,7 +7,15 @@
 # chmod 600 ~/.netrc
 
 #----------------Installation-Steps----------------------
-# 1. sudo apt-get inotify-tools ftp -y
+# 1. sudo apt-get inotify-tools ftp git -y --force-yes
+# 2. git clone https://Zamanry/2019Fall_CNIT581_IOT.git
+# 3. crontab -e
+# 4. 	SHELL=/bin/bash
+# 5. 	MAILTO=root@example.com
+# 6. 	PATH=/root
+# 7.	0 * * * * /root/2019Fall_CNIT581_IOT/DVR-to-Pi.sh
+# 8. chmod +X 2019Fall_CNIT581_IOT/DVR-to-Pi.sh
+# 9. chown root:root DVR-to-Pi.sh
 
 #----------------Overall-Steps---------------------------
 # 1. Detect a new JSON was created per each camera on current date
@@ -32,7 +40,7 @@ Date=""
 	# Establish a global Date variable
 #----------------Functions------------------------------
 FTPToPi() {
-	Ftp -4 "$EdgeServer"
+	ftp -4 "$EdgeServer"
 	cd "$EdgeMotionDir/$Date"
 	put "$1"
 	exit
@@ -43,7 +51,8 @@ FTPToPi() {
 		# Rename it
 }
 MotionCheck() {
-	inotifywait -e created -t 20 "$1/meta/*.json" | while read -r "NewJSON"; do
+	inotifywait -e create -t 20 --exclude '\.(jpg|png)' --format '%f' "$1/meta/*.json" | while read -r "newjson"; do
+		echo "Created file was named $newjson."
 			# Checks recursively for 20 seconds if any file has been created
 		Date=$(date +%m-%d-%Y-%H:%M)
 				# Get date in a nice format (ex.11-20-2020-17:14)
@@ -51,14 +60,14 @@ MotionCheck() {
 			# Since a new motion (JSON) was found, send a notification to edge server
 		cd "$1" || exit
 			# Just changing directories
-		StartTime=$(grep 'startTime":(.*?)\,' "meta/$NewJSON")
+		StartTime=$(grep -Po 'startTime":(.*?),' "meta/$newjson" | sed -n 's/.*://p' | sed 's/,$//')
 			# Obtain motion's startTime from new JSON
-		Ftp -4 "$EdgeServer"
+		ftp -4 -n "$EdgeServer"
 		cd "$EdgeMotionDir"
 		mkdir "$Date"
-		exit
+		quit
 			# Make a directory with the timestamp in the Pi's motion folder
-		while (grep 'inProgress":(.*?)\,' "meta/$NewJSON" -eq 'true'); do
+		while ($(grep -Po 'inProgress":(.*?),' "meta/$newjson" | sed -n 's/.*://p' | sed 's/,$//') -eq 'true'); do
 			# Detect if motion is still in progress according to the JSON
 			Videos=$(grep -v "_FTPed\.mp4")
 				# Grab all videos that do not contain _FTPed.mp4
@@ -70,7 +79,7 @@ MotionCheck() {
 				fi
 			done
 		done
-		EndTime=$(grep 'endTime":(.*?)\,' "meta/$NewJSON")
+		EndTime=$(grep -Po 'endTime":(.*?),' "meta/$newjson" | sed -n 's/.*://p' | sed 's/,$//')
 			# Obtain motion's endTime from new JSON
 		Videos=$(grep -v "_FTPed\.mp4")
 			# Grab all videos that do not contain _FTPed.mp4
@@ -84,9 +93,9 @@ MotionCheck() {
 	done
 }
 SendNotification() {
-	Touch "$Date.txt"
+	echo "$null" >> "$Date.txt"
 		# Create notification message to alert the farmer rather than wait for the large video file to transfer (ex.11-20-2020-17:14.txt)
-	Ftp -4 "$EdgeServer"
+	ftp -4 "$EdgeServer"
 	cd "$EdgeNotificationsDir"
 	put "./$Date.txt"
 	exit
@@ -102,13 +111,13 @@ if [ -z "$(pgrep -f "DVR-to-Pi.sh")" ]; then
 		# Just changing directories
 	while $T -e "true"; do
 			# Run forever
-		Year=$(date +%Y)
+		Year=$(date +%Y -u)
 			# Get year (ex. 2019)
-		Month=$(date +%m)
+		Month=$(date +%m -u)
 			# Get month (ex. 11)
-		Day=$(date +%d)
+		Day=$(date +%d -u)
 			# Get day (ex. 27)
-		Cameras=$(grep '^d' "$UniFiDir")
+		Cameras=$(ls "$UniFiDir" | grep '^d' )
 			# Get each cameras' directory and assign it to $Cameras
 		ctr='0'
 		for Camera in $Cameras; do
@@ -116,7 +125,7 @@ if [ -z "$(pgrep -f "DVR-to-Pi.sh")" ]; then
 				# Add cameras' path to array $CameraPath
 			MotionCheck "${CameraPath[$ctr]}" &
 				# Check for motion (JSON files) at path and run in the background
-			$ctr++
+			ctr="$ctr + 1"
 		done
 		Wait 21
 			# Wait for (inotifywait_time + 1) seconds to check for a new date
